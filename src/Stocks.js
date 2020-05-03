34aa2cb4565ff285ebb02cd1f0a0ebd569e3cdd9
Jonathan Montoya <montoya0396@gmail.com>
2020-05-03 15:00:41 -0400

    added chart, still need to fix API call when state of stockSymbol changes to be able to change ticker displayed on graph

--- src/Stocks.js
+++ src/Stocks.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import createPlotlyComponent from 'react-plotly.js/factory';
+
 
 
 export default class Stocks extends React.Component {
@@ -18,6 +20,7 @@ export default class Stocks extends React.Component {
     {
         e.preventDefault();
         this.setState({ stockSymbol: this.textInput.current.value})
+        this.componentDidMount();
     };
 
     componentDidMount()
@@ -27,8 +30,8 @@ export default class Stocks extends React.Component {
 
     fetchStock()
     {
-        const pointerToThis = this;
         let API_CALL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${this.state.stockSymbol}&apikey=HC59HK11T8SB44OT`;
+        const pointerToThis = this;
         let stockCartXValuesFunction = [];
         let stockCartYValuesFunction = [];
 
@@ -62,12 +65,33 @@ export default class Stocks extends React.Component {
 
     render()
     {
+        const Plotly = window.Plotly;
+        const Plot = createPlotlyComponent(Plotly);
+        var ticker = this.state.stockSymbol;
+
         return (
             <div>
-                <h1>Ticker: {this.state.stockSymbol} </h1>
+                <h1>Ticker: {ticker} </h1>
                 
                 <input type="text" ref={this.textInput}/>
                 <button onClick={this.handleSubmit}>Change Ticker</button>
+                <br/>
+                <br/>
+                <br/>
+
+                <Plot
+                    data=
+                    {[
+                        {
+                            x: this.state.stockChartXValues,
+                            y: this.state.stockCartYValues,
+                            type: 'scatter',
+                            mode: 'lines+markers',
+                            marker: {color: 'red'},
+                        },
+                    ]}
+                    layout={ {width: 1200, height: 500 } }
+                />
             </div>
         );
 
